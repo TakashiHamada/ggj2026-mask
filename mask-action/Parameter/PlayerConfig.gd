22454449f09558efa8ca72d53411c7b9571779a2
Tristan Ansel Angeles <caladan21ph@gmail.com>
2026-01-31 12:55:22 +0800

    Added more zombies
    Added zombies, and increase player jump height.expanded level.

--- mask-action/Parameter/PlayerConfig.gd
+++ mask-action/Parameter/PlayerConfig.gd
@@ -3,5 +3,5 @@ class_name PlayerConfig
 
 const MOVE_SPEED: float = 150.0
 const MOVE_SPEED_WITH_MASK: float = 80.0  # マスク装着時の移動速度
-const JUMP_VELOCITY: float = -250.0
-const GAS_DAMAGE_PER_SECOND: float = 1.0  # 毒ガスで1秒あたりに受けるダメージ
+const JUMP_VELOCITY: float = -350.0
+const GAS_DAMAGE_PER_SECOND: float = .1  # 毒ガスで1秒あたりに受けるダメージ
