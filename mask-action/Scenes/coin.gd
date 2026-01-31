9343dbd16a8c81865e20ab1f1679c60937b6ee05
Tristan Ansel Angeles <caladan21ph@gmail.com>
2026-01-31 23:28:11 +0800

    Coin get sfx
    added coin get sfx

--- mask-action/Scenes/coin.gd
+++ mask-action/Scenes/coin.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
-@export var value: int = 1
+@onready var sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-func _ready():
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
+func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		body.add_coin(value)
+		sfx.play()
+		
+		# Hide coin immediately
+		visible = false
+		set_deferred("monitoring", false)
+
+		# Wait for sound to finish, then delete
+		await sfx.finished
 		queue_free()
